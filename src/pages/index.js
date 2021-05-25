bb4223cb32da537bd63e11abfd6f6c0fb1907ff0
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-05-25 17:57:31 -0600

    configured tailwind css

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,22 +7,24 @@ import Seo from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    <h1 className="text-4xl text-blue-900">Calling all truck drivers</h1>
+    {/* Hero */}
+    {/* We work with the best */}
+
+    {/* Who are we? */}
+      {/* What do we do? */}
+    {/* Why work with us? */}
+    
+
+    {/* Employee Driver and Owner Operator */}
+    {/* Pay */}
+    {/* How to get started */}
+    {/* FAQ */}
+    {/* Open Positions */}
+      {/* Employee  */}
+        {/* Link to tentstreet */}
+      {/* Owner/operator */}
+        {/* Calendly Link */}
   </Layout>
 )
 
