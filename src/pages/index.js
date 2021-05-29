56584852ba7d98108b9bb65462f35772ee1222f6
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-05-28 21:25:51 -0600

    Update index.js
    Imported out all of the components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,50 @@
-import * as React from 'react'
-import { Link } from 'gatsby'
-import { StaticImage } from 'gatsby-plugin-image'
+import * as React from 'react';
+import { Link } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 
-import Layout from '../components/layout'
-import Seo from '../components/seo'
+import Layout from '../components/layout';
+import Seo from '../components/seo';
+import { Hero } from '../components/hero';
+import { WhyDriveForUs } from '../components/why-drive-for-us';
+import { EmployeeVSOwner } from '../components/employee-vs-owner';
+import { Brands } from '../components/brands';
+import { FAQ } from '../components/FAQ';
+import { Testimonials } from '../components/testimonials';
+import { ApplicationProcess } from '../components/application-process';
+import { JobOpenings } from '../components/job-openings';
 
 const IndexPage = () => (
   <Layout>
-    <h1 className="text-4xl text-blue-900">Calling all truck drivers</h1>
     {/* Hero */}
-    {/* We work with the best */}
+    <Hero />
 
-    {/* Who are we? */}
-    {/* What do we do? */}
+    {/* Who are we? and What do we do? */}
+
     {/* Why work with us? */}
+    <WhyDriveForUs />
 
-    {/* Employee Driver and Owner Operator */}
-    {/* Pay */}
+    {/* How we work with Drivers and Owner Operators */}
+    <EmployeeVSOwner />
+
+    {/* Brands we work with logos */}
+    <Brands />
+
+    {/* Testimonials */}
+    <Testimonials />
+
     {/* How to get started */}
+    <ApplicationProcess />
+
     {/* FAQ */}
+    <FAQ />
+
+    <JobOpenings />
     {/* Open Positions */}
     {/* Employee  */}
     {/* Link to tentstreet */}
     {/* Owner/operator */}
     {/* Calendly Link */}
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
