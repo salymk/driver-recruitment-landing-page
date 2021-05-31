c294a2591e943b3daccdb686f65681860587ce1b
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-05-31 16:50:51 -0600

    Added buttons and titles

--- src/components/application-process.js
+++ src/components/application-process.js
@@ -2,88 +2,122 @@ import React from 'react';
 
 export const ApplicationProcess = () => (
   <>
-    <section className="section-spacing lg:pb-24">
-      <div className="container px-4 mx-auto">
-        <div className="mb-8">
-          <h2 className="text-center text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            The process for working with us
-          </h2>
-          <p className="mt-4 max-w-3xl mx-auto text-center text-xl text-gray-500">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
-            magnam voluptatum cupiditate veritatis in, accusamus quisquam.
-          </p>
-        </div>
-        <div className="flex flex-wrap -mx-4">
-          <div className="w-full lg:w-1/2 px-4 py-4">
-            <div className="max-w-max mx-auto bg-gray-900 text-white rounded-lg p-6 md:p-12">
-              <div className="flex items-start mb-6">
-                <span className="mr-10 flex-shrink-0 flex justify-center items-center w-12 h-12 rounded-full bg-blue-500 text-white font-bold font-heading">
-                  1
-                </span>
-                <div className="max-w-xs">
-                  <p className="text-lg leading-loose">
-                    Etiam pellentesque non nibh non pulvinar. Mauris posuere,
-                    tellus sit amet tempus vestibulum.
-                  </p>
-                </div>
-              </div>
-              <div className="flex items-start mb-6">
-                <span className="mr-10 flex-shrink-0 flex justify-center items-center w-12 h-12 rounded-full bg-blue-500 text-white font-bold font-heading">
-                  2
-                </span>
-                <div className="max-w-xs">
-                  <p className="text-lg leading-loose">
-                    Etiam pellentesque non nibh non pulvinar. Mauris posuere,
-                    tellus sit amet tempus vestibulum.
-                  </p>
-                </div>
-              </div>
-              <div className="flex items-start">
-                <span className="mr-10 flex-shrink-0 flex justify-center items-center w-12 h-12 rounded-full bg-blue-500 text-white font-bold font-heading">
-                  3
-                </span>
-                <div className="max-w-xs">
-                  <p className="text-lg leading-loose">
-                    Etiam pellentesque non nibh non pulvinar. Mauris posuere,
-                    tellus sit amet tempus vestibulum.
-                  </p>
+    <section className="relative bg-white section-spacing lg:pb-28">
+      <div className="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-0 lg:max-w-7xl">
+        <p className="mt-2 text-3xl font-bold text-gray-900 tracking-tight sm:text-4xl">
+          Application Process
+        </p>
+        <p className="mt-5 max-w-prose mx-auto text-xl text-gray-500">
+          Phasellus lorem quam molestie id quisque diam aenean nulla in.
+          Accumsan in quis quis nunc, ullamcorper malesuada. Eleifend
+          condimentum id viverra nulla.
+        </p>
+        <div className="mt-12">
+          <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-2">
+            <div className="pt-6">
+              <div className="flow-root bg-gray-900 rounded-lg shadow-lg px-6 pb-8">
+                <div className="-mt-6">
+                  <div>
+                    <span className="inline-flex items-center justify-center p-3 bg-blue-500 rounded-md shadow-lg">
+                      <h3 className="mb-0 text-3xl font-medium text-white tracking-tight">
+                        Company Driver
+                      </h3>
+                    </span>
+                  </div>
+                  <div className="flex flex-col justify-between text-left py-8 text-white">
+                    <div className="flex items-start mb-6">
+                      <span className="mr-10 flex-shrink-0 flex justify-center items-center w-12 h-12 rounded-full bg-blue-500 text-white font-bold font-heading">
+                        1
+                      </span>
+                      <div className="max-w-xs">
+                        <p className="text-lg leading-loose">
+                          Etiam pellentesque non nibh non pulvinar. Mauris
+                          posuere, tellus sit amet tempus vestibulum.
+                        </p>
+                      </div>
+                    </div>
+                    <div className="flex items-start mb-6">
+                      <span className="mr-10 flex-shrink-0 flex justify-center items-center w-12 h-12 rounded-full bg-blue-500 text-white font-bold font-heading">
+                        2
+                      </span>
+                      <div className="max-w-xs">
+                        <p className="text-lg leading-loose">
+                          Etiam pellentesque non nibh non pulvinar. Mauris
+                          posuere, tellus sit amet tempus vestibulum.
+                        </p>
+                      </div>
+                    </div>
+                    <div className="flex items-start">
+                      <span className="mr-10 flex-shrink-0 flex justify-center items-center w-12 h-12 rounded-full bg-blue-500 text-white font-bold font-heading">
+                        3
+                      </span>
+                      <div className="max-w-xs">
+                        <p className="text-lg leading-loose">
+                          Etiam pellentesque non nibh non pulvinar. Mauris
+                          posuere, tellus sit amet tempus vestibulum.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="rounded-md shadow">
+                    <a className="cursor-pointer uppercase w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-blue-700 bg-gray-50 hover:bg-gray-200 md:py-4 md:text-md md:px-10">
+                      Apply Now
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="w-full lg:w-1/2 px-4 py-4">
-            <div className="max-w-max mx-auto bg-gray-100 text-gray-900 rounded-lg p-6 md:p-12">
-              <div className="flex items-start mb-6">
-                <span className="mr-10 flex-shrink-0 flex justify-center items-center w-12 h-12 rounded-full bg-blue-500 text-white font-bold font-heading">
-                  1
-                </span>
-                <div className="max-w-xs">
-                  <p className="text-lg leading-loose">
-                    Etiam pellentesque non nibh non pulvinar. Mauris posuere,
-                    tellus sit amet tempus vestibulum.
-                  </p>
-                </div>
-              </div>
-              <div className="flex items-start mb-6">
-                <span className="mr-10 flex-shrink-0 flex justify-center items-center w-12 h-12 rounded-full bg-blue-500 text-white font-bold font-heading">
-                  2
-                </span>
-                <div className="max-w-xs">
-                  <p className="text-lg leading-loose">
-                    Etiam pellentesque non nibh non pulvinar. Mauris posuere,
-                    tellus sit amet tempus vestibulum.
-                  </p>
-                </div>
-              </div>
-              <div className="flex items-start">
-                <span className="mr-10 flex-shrink-0 flex justify-center items-center w-12 h-12 rounded-full bg-blue-500 text-white font-bold font-heading">
-                  3
-                </span>
-                <div className="max-w-xs">
-                  <p className="text-lg leading-loose">
-                    Etiam pellentesque non nibh non pulvinar. Mauris posuere,
-                    tellus sit amet tempus vestibulum.
-                  </p>
+
+            <div className="pt-6">
+              <div className="flow-root bg-gray-100 rounded-lg shadow-lg px-6 pb-8">
+                <div className="-mt-6">
+                  <div>
+                    <span className="inline-flex items-center justify-center p-3 bg-gray-900 rounded-md shadow-lg">
+                      <h3 className="mb-0 text-3xl font-medium text-white tracking-tight">
+                        Owner Operator
+                      </h3>
+                    </span>
+                  </div>
+                  <div className="flex flex-col justify-between text-left py-8 text-gray-900">
+                    <div className="flex items-start mb-6">
+                      <span className="mr-10 flex-shrink-0 flex justify-center items-center w-12 h-12 rounded-full bg-blue-500 text-white font-bold font-heading">
+                        1
+                      </span>
+                      <div className="max-w-xs">
+                        <p className="text-lg leading-loose">
+                          Etiam pellentesque non nibh non pulvinar. Mauris
+                          posuere, tellus sit amet tempus vestibulum.
+                        </p>
+                      </div>
+                    </div>
+                    <div className="flex items-start mb-6">
+                      <span className="mr-10 flex-shrink-0 flex justify-center items-center w-12 h-12 rounded-full bg-blue-500 text-white font-bold font-heading">
+                        2
+                      </span>
+                      <div className="max-w-xs">
+                        <p className="text-lg leading-loose">
+                          Etiam pellentesque non nibh non pulvinar. Mauris
+                          posuere, tellus sit amet tempus vestibulum.
+                        </p>
+                      </div>
+                    </div>
+                    <div className="flex items-start">
+                      <span className="mr-10 flex-shrink-0 flex justify-center items-center w-12 h-12 rounded-full bg-blue-500 text-white font-bold font-heading">
+                        3
+                      </span>
+                      <div className="max-w-xs">
+                        <p className="text-lg leading-loose">
+                          Etiam pellentesque non nibh non pulvinar. Mauris
+                          posuere, tellus sit amet tempus vestibulum.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="rounded-md shadow">
+                    <a className="cursor-pointer uppercase w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-600 md:py-4 md:text-md md:px-10">
+                      Apply Now
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
@@ -93,3 +127,93 @@ export const ApplicationProcess = () => (
     </section>
   </>
 );
+
+//   <section className="section-spacing lg:pb-24">
+// <div className="container px-4 mx-auto">
+//   <div className="mb-8">
+//     <h2 className="text-center text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+//       The process for working with us
+//     </h2>
+//     <p className="mt-4 max-w-3xl mx-auto text-center text-xl text-gray-500">
+//       Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+//       magnam voluptatum cupiditate veritatis in, accusamus quisquam.
+//     </p>
+//   </div>
+//   <div className="flex flex-wrap -mx-4">
+//     <div className="w-full lg:w-1/2 px-4 py-4">
+//       <div className="max-w-max mx-auto bg-gray-900 text-white rounded-lg p-6 md:p-12">
+//         <div className="flex items-start mb-6">
+//           <span className="mr-10 flex-shrink-0 flex justify-center items-center w-12 h-12 rounded-full bg-blue-500 text-white font-bold font-heading">
+//             1
+//           </span>
+//           <div className="max-w-xs">
+//             <p className="text-lg leading-loose">
+//               Etiam pellentesque non nibh non pulvinar. Mauris posuere,
+//               tellus sit amet tempus vestibulum.
+//             </p>
+//           </div>
+//         </div>
+//         <div className="flex items-start mb-6">
+//           <span className="mr-10 flex-shrink-0 flex justify-center items-center w-12 h-12 rounded-full bg-blue-500 text-white font-bold font-heading">
+//             2
+//           </span>
+//           <div className="max-w-xs">
+//             <p className="text-lg leading-loose">
+//               Etiam pellentesque non nibh non pulvinar. Mauris posuere,
+//               tellus sit amet tempus vestibulum.
+//             </p>
+//           </div>
+//         </div>
+//         <div className="flex items-start">
+//           <span className="mr-10 flex-shrink-0 flex justify-center items-center w-12 h-12 rounded-full bg-blue-500 text-white font-bold font-heading">
+//             3
+//           </span>
+//           <div className="max-w-xs">
+//             <p className="text-lg leading-loose">
+//               Etiam pellentesque non nibh non pulvinar. Mauris posuere,
+//               tellus sit amet tempus vestibulum.
+//             </p>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//     <div className="w-full lg:w-1/2 px-4 py-4">
+//       <div className="max-w-max mx-auto bg-gray-100 text-gray-900 rounded-lg p-6 md:p-12">
+//         <div className="flex items-start mb-6">
+//           <span className="mr-10 flex-shrink-0 flex justify-center items-center w-12 h-12 rounded-full bg-blue-500 text-white font-bold font-heading">
+//             1
+//           </span>
+//           <div className="max-w-xs">
+//             <p className="text-lg leading-loose">
+//               Etiam pellentesque non nibh non pulvinar. Mauris posuere,
+//               tellus sit amet tempus vestibulum.
+//             </p>
+//           </div>
+//         </div>
+//         <div className="flex items-start mb-6">
+//           <span className="mr-10 flex-shrink-0 flex justify-center items-center w-12 h-12 rounded-full bg-blue-500 text-white font-bold font-heading">
+//             2
+//           </span>
+//           <div className="max-w-xs">
+//             <p className="text-lg leading-loose">
+//               Etiam pellentesque non nibh non pulvinar. Mauris posuere,
+//               tellus sit amet tempus vestibulum.
+//             </p>
+//           </div>
+//         </div>
+//         <div className="flex items-start">
+//           <span className="mr-10 flex-shrink-0 flex justify-center items-center w-12 h-12 rounded-full bg-blue-500 text-white font-bold font-heading">
+//             3
+//           </span>
+//           <div className="max-w-xs">
+//             <p className="text-lg leading-loose">
+//               Etiam pellentesque non nibh non pulvinar. Mauris posuere,
+//               tellus sit amet tempus vestibulum.
+//             </p>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+// </div>
+// </section>
