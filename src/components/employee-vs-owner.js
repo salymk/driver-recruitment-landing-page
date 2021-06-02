b8055a5688f1cc39ecc1c63f63aa9472899ae3ed
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-06-01 19:04:03 -0600

    Added new benefits with checkmark svg

--- src/components/employee-vs-owner.js
+++ src/components/employee-vs-owner.js
@@ -25,99 +25,105 @@ export const EmployeeVSOwner = () => (
               laborum ab aliquid veritatis impedit odit adipisci optio iste
               blanditiis facere. Totam, velit.
             </p>
-
-            <dl className="mt-10 space-y-10">
+            <dl className="mt-10 space-y-4">
               <div className="relative">
                 <dt>
-                  <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-gray-900 text-white">
-                    {/* <!-- Heroicon name: outline/globe-alt --> */}
+                  <div className="absolute flex items-center justify-center text-blue-500">
+                    {/* <!-- Heroicon name: outline/mail --> */}
                     <svg
-                      className="h-6 w-6"
                       xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
                       fill="none"
                       viewBox="0 0 24 24"
                       stroke="currentColor"
-                      aria-hidden="true"
                     >
                       <path
                         strokeLinecap="round"
                         strokeLinejoin="round"
                         strokeWidth="2"
-                        d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
+                        d="M5 13l4 4L19 7"
                       />
                     </svg>
                   </div>
-                  <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
-                    Competitive exchange rates
+                  <p className="ml-12 text-lg leading-6 font-medium text-gray-900">
+                    Owner Operators must have their own trucks
                   </p>
                 </dt>
-                <dd className="mt-2 ml-16 text-base text-gray-500">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Maiores impedit perferendis suscipit eaque, iste dolor
-                  cupiditate blanditiis ratione.
-                </dd>
               </div>
 
               <div className="relative">
                 <dt>
-                  <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-gray-900 text-white">
-                    {/* <!-- Heroicon name: outline/scale --> */}
+                  <div className="absolute flex items-center justify-center text-blue-500">
+                    {/* <!-- Heroicon name: outline/mail --> */}
                     <svg
-                      className="h-6 w-6"
                       xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
                       fill="none"
                       viewBox="0 0 24 24"
                       stroke="currentColor"
-                      aria-hidden="true"
                     >
                       <path
                         strokeLinecap="round"
                         strokeLinejoin="round"
                         strokeWidth="2"
-                        d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"
+                        d="M5 13l4 4L19 7"
                       />
                     </svg>
                   </div>
-                  <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
-                    No hidden fees
+                  <p className="ml-12 text-lg leading-6 font-medium text-gray-900">
+                    Up to $5,000 Sign on Bonus for OTR O/O
                   </p>
                 </dt>
-                <dd className="mt-2 ml-16 text-base text-gray-500">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Maiores impedit perferendis suscipit eaque, iste dolor
-                  cupiditate blanditiis ratione.
-                </dd>
               </div>
 
               <div className="relative">
                 <dt>
-                  <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-gray-900 text-white">
-                    {/* <!-- Heroicon name: outline/lightning-bolt --> */}
+                  <div className="absolute flex items-center justify-center text-blue-500">
+                    {/* <!-- Heroicon name: outline/mail --> */}
                     <svg
-                      className="h-6 w-6"
                       xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
                       fill="none"
                       viewBox="0 0 24 24"
                       stroke="currentColor"
-                      aria-hidden="true"
                     >
                       <path
                         strokeLinecap="round"
                         strokeLinejoin="round"
                         strokeWidth="2"
-                        d="M13 10V3L4 14h7v7l9-11h-7z"
+                        d="M5 13l4 4L19 7"
                       />
                     </svg>
                   </div>
-                  <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
-                    Transfers are instant
+                  <p className="ml-12 text-lg leading-6 font-medium text-gray-900">
+                    90% revenue percentage pay
                   </p>
                 </dt>
-                <dd className="mt-2 ml-16 text-base text-gray-500">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Maiores impedit perferendis suscipit eaque, iste dolor
-                  cupiditate blanditiis ratione.
-                </dd>
+              </div>
+
+              <div className="relative">
+                <dt>
+                  <div className="absolute flex items-center justify-center text-blue-500">
+                    {/* <!-- Heroicon name: outline/mail --> */}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M5 13l4 4L19 7"
+                      />
+                    </svg>
+                  </div>
+                  <p className="ml-12 text-lg leading-6 font-medium text-gray-900">
+                    Owner Operators must have their own trucks
+                  </p>
+                </dt>
               </div>
             </dl>
           </div>
@@ -136,7 +142,7 @@ export const EmployeeVSOwner = () => (
           <div className="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:gap-8 lg:items-center">
             <div className="lg:col-start-2">
               <h3 className="text-2xl font-bold text-gray-900 tracking-tight sm:text-3xl">
-                Driver
+                Company Driver
               </h3>
               <p className="mt-3 text-lg text-gray-500">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
@@ -144,67 +150,80 @@ export const EmployeeVSOwner = () => (
                 labore nihil quod quibusdam expedita nemo.
               </p>
 
-              <dl className="mt-10 space-y-10">
+              <dl className="mt-10 space-y-4">
                 <div className="relative">
                   <dt>
-                    <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-gray-900 text-white">
-                      {/* <!-- Heroicon name: outline/annotation --> */}
+                    <div className="absolute flex items-center justify-center text-blue-500">
+                      {/* <!-- Heroicon name: outline/mail --> */}
                       <svg
-                        className="h-6 w-6"
                         xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
                         fill="none"
                         viewBox="0 0 24 24"
                         stroke="currentColor"
-                        aria-hidden="true"
                       >
                         <path
                           strokeLinecap="round"
                           strokeLinejoin="round"
                           strokeWidth="2"
-                          d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
+                          d="M5 13l4 4L19 7"
                         />
                       </svg>
                     </div>
-                    <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
-                      Mobile notifications
+                    <p className="ml-12 text-lg leading-6 font-medium text-gray-900">
+                      Owner Operators must have their own trucks
                     </p>
                   </dt>
-                  <dd className="mt-2 ml-16 text-base text-gray-500">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Maiores impedit perferendis suscipit eaque, iste dolor
-                    cupiditate blanditiis ratione.
-                  </dd>
                 </div>
 
                 <div className="relative">
                   <dt>
-                    <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-gray-900 text-white">
+                    <div className="absolute flex items-center justify-center text-blue-500">
                       {/* <!-- Heroicon name: outline/mail --> */}
                       <svg
-                        className="h-6 w-6"
                         xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
                         fill="none"
                         viewBox="0 0 24 24"
                         stroke="currentColor"
-                        aria-hidden="true"
                       >
                         <path
                           strokeLinecap="round"
                           strokeLinejoin="round"
                           strokeWidth="2"
-                          d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                          d="M5 13l4 4L19 7"
                         />
                       </svg>
                     </div>
-                    <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
-                      Reminder emails
+                    <p className="ml-12 text-lg leading-6 font-medium text-gray-900">
+                      Up to $5,000 Sign on Bonus for OTR O/O
                     </p>
                   </dt>
-                  <dd className="mt-2 ml-16 text-base text-gray-500">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Maiores impedit perferendis suscipit eaque, iste dolor
-                    cupiditate blanditiis ratione.
-                  </dd>
+                </div>
+
+                <div className="relative">
+                  <dt>
+                    <div className="absolute flex items-center justify-center text-blue-500">
+                      {/* <!-- Heroicon name: outline/mail --> */}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M5 13l4 4L19 7"
+                        />
+                      </svg>
+                    </div>
+                    <p className="ml-12 text-lg leading-6 font-medium text-gray-900">
+                      90% revenue percentage pay
+                    </p>
+                  </dt>
                 </div>
               </dl>
             </div>
