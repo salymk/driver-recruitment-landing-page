446179460b6fd6291bd426575835313e9deed18e
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-06-02 20:23:20 -0600

    Added width to images

--- src/components/employee-vs-owner.js
+++ src/components/employee-vs-owner.js
@@ -3,8 +3,8 @@ import React from 'react';
 export const EmployeeVSOwner = () => (
   <>
     <section className="overflow-hidden section-spacing">
-      <div className="relative max-w-xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
-        <div className="relative">
+      <div className="relative max-w-xl mx-auto sm:px-6 lg:px-8 lg:max-w-7xl">
+        <div className="relative px-4">
           <h2 className="text-center text-3xl leading-8 font-bold tracking-tight text-gray-900 sm:text-4xl">
             How we work drivers and owner-operators
           </h2>
@@ -131,9 +131,9 @@ export const EmployeeVSOwner = () => (
           <div className="mt-10 -mx-4 relative lg:mt-0" aria-hidden="true">
             <img
               className="relative mx-auto sm:rounded-lg"
-              width="490"
               src="https://images.unsplash.com/photo-1592805144716-feeccccef5ac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
               alt=""
+              width="490"
             />
           </div>
         </div>
@@ -225,15 +225,39 @@ export const EmployeeVSOwner = () => (
                     </p>
                   </dt>
                 </div>
+                <div className="relative">
+                  <dt>
+                    <div className="absolute flex items-center justify-center text-blue-500">
+                      {/* <!-- Heroicon name: outline/mail --> */}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M5 13l4 4L19 7"
+                        />
+                      </svg>
+                    </div>
+                    <p className="ml-12 text-lg leading-6 font-medium text-gray-900">
+                      90% revenue percentage pay
+                    </p>
+                  </dt>
+                </div>
               </dl>
             </div>
 
             <div className="mt-10 -mx-4 relative lg:mt-0 lg:col-start-1">
               <img
                 className="relative mx-auto sm:rounded-lg"
-                width="490"
                 src="https://images.unsplash.com/photo-1592805144716-feeccccef5ac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
                 alt=""
+                width="490"
               />
             </div>
           </div>
