b7c0253b88b4a58049d03c3ebb7e06a4dbf0496b
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-06-02 20:23:54 -0600

    Added margin left an right

--- src/components/job-openings.js
+++ src/components/job-openings.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const JobOpenings = () => (
   <>
-    <section className="relative bg-white section-spacing">
+    <section className="relative bg-white mx-4 section-spacing">
       <div className="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-0 lg:max-w-7xl">
         <p className="mt-2 text-3xl font-bold text-gray-900 tracking-tight sm:text-4xl">
           Open roles
