e87de07e0d398871ea83b201e25957149576a2ea
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-06-02 19:47:21 -0600

    Added px-4

--- src/components/why-drive-for-us.js
+++ src/components/why-drive-for-us.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export const WhyDriveForUs = () => (
   <>
-    <section className="relative bg-white section-spacing">
-      <div className="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
+    <section className="relative bg-white px-4 section-spacing">
+      <div className="mx-auto max-w-md text-center sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
         <p className="mt-2 text-3xl font-bold text-gray-900 tracking-tight sm:text-4xl">
           Why drive for us?
         </p>
