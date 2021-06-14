f6c4cd8e8f18284189ca79ba38586d1f3192b3cb
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-06-13 20:01:21 -0600

    Pulled data from backend

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,56 +1,87 @@
 import React from 'react';
 import { Link as LinkScroll } from 'react-scroll';
+import { graphql, useStaticQuery } from 'gatsby';
 
-export const Hero = () => (
-  <>
-    <section id="Home">
-      <div className="px-4 mx-auto mt-24">
-        <div className="pt-6 lg:pt-0 flex flex-wrap items-center -mx-4 lg:my-10">
-          <div className="text-center w-full md:text-left md:w-1/2 px-4 mb-8 md:mb-0">
-            <h2 className="mb-8 text-3xl lg:text-4xl font-bold lg:max-w-sm">
-              Take care of your performance every day. performance every day
-            </h2>
-            <p className="mb-6 text-lg text-gray-500 lg:max-w-lg">
-              Build a well-presented brand that everyone will love. Take care to
-              develop resources continually and integrity them with previous
-              projects.
-            </p>
-            <div className="mt-5 sm:mt-8 sm:flex sm:justify-center md:justify-left lg:justify-start">
-              <div className="rounded-md shadow">
-                <LinkScroll
-                  to="Application"
-                  spy
-                  smooth
-                  offset={-95}
-                  duration={1000}
-                  className="cursor-pointer uppercase w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-600 sm:text-sm lg:text-lg md:py-4 lg:text-md md:px-10"
-                >
-                  Apply Now
-                </LinkScroll>
-              </div>
-              <div className="mt-3 sm:mt-0 sm:ml-3">
-                <LinkScroll
-                  to="Benefits"
-                  spy
-                  smooth
-                  offset={-95}
-                  duration={1000}
-                  className="cursor-pointer uppercase w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-blue-700 bg-blue-100 hover:bg-blue-200 sm:text-sm lg:text-lg md:py-4 lg:text-md md:px-10"
-                >
-                  Learn More
-                </LinkScroll>
+export const Hero = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      wpPage(title: { eq: "Recruitment Landing Page" }) {
+        recruitment {
+          hero {
+            to
+            title
+            description
+            ctaButtons {
+              apply
+              learn
+            }
+            image {
+              altText
+              sourceUrl
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const {
+    to,
+    title,
+    description,
+    ctaButtons,
+    image,
+  } = data.wpPage.recruitment.hero;
+  console.log(data);
+  return (
+    <>
+      <section id={to}>
+        <div className="px-4 mx-auto mt-24">
+          <div className="pt-6 lg:pt-0 flex flex-wrap items-center -mx-4 lg:my-10">
+            <div className="text-center w-full md:text-left md:w-1/2 px-4 mb-8 md:mb-0">
+              <h2 className="mb-8 text-3xl lg:text-4xl font-bold lg:max-w-sm">
+                {title}
+              </h2>
+              <p className="mb-6 text-lg text-gray-500 lg:max-w-lg">
+                {description}
+              </p>
+              <div className="mt-5 sm:mt-8 sm:flex sm:justify-center md:justify-left lg:justify-start">
+                <div className="rounded-md shadow">
+                  <LinkScroll
+                    to="Application"
+                    spy
+                    smooth
+                    offset={-95}
+                    duration={1000}
+                    className="cursor-pointer uppercase w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-600 sm:text-sm lg:text-lg md:py-4 lg:text-md md:px-10"
+                  >
+                    {ctaButtons.apply}
+                  </LinkScroll>
+                </div>
+                <div className="mt-3 sm:mt-0 sm:ml-3">
+                  <LinkScroll
+                    to="Benefits"
+                    spy
+                    smooth
+                    offset={-95}
+                    duration={1000}
+                    className="cursor-pointer uppercase w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-blue-700 bg-blue-100 hover:bg-blue-200 sm:text-sm lg:text-lg md:py-4 lg:text-md md:px-10"
+                  >
+                    {ctaButtons.learn}
+                  </LinkScroll>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="mx-auto w-full h-full md:px-4 md:w-1/2">
-            <img
-              className="object-cover w-full sm:rounded-lg"
-              src="https://images.unsplash.com/photo-1561157437-415893bd7149?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1165&q=80"
-              alt=""
-            />
+            <div className="mx-auto w-full h-full md:px-4 md:w-1/2">
+              <img
+                className="object-cover w-full sm:rounded-lg"
+                src={image.sourceUrl}
+                alt={image.altText}
+              />
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-  </>
-);
+      </section>
+    </>
+  );
+};
--- src/components/why-drive-for-us.js
+++ src/components/why-drive-for-us.js
@@ -1,127 +1,61 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 
-export const WhyDriveForUs = () => (
-  <>
-    <section className="relative bg-white px-4 section-spacing" id="Benefits">
-      <div className="mx-auto max-w-md text-center sm:max-w-3xl lg:max-w-7xl">
-        <p className="mt-2 text-3xl font-bold tracking-tight sm:text-4xl">
-          Why drive for us?
-        </p>
-        <p className="mt-5 max-w-prose mx-auto text-xl text-gray-500">
-          Phasellus lorem quam molestie id quisque diam aenean nulla in.
-          Accumsan in quis quis nunc, ullamcorper malesuada. Eleifend
-          condimentum id viverra nulla.
-        </p>
-        <div className="mt-12">
-          <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
-            <div className="pt-6 group">
-              <div className="flow-root  bg-gray-900 rounded-lg px-6 pb-8 transition duration-500 ease-in-out group-hover:border-2 group-hover:bg-white group-hover:border-gray-100 group-hover:shadow-xl">
-                <div className="-mt-6">
-                  <div>
-                    <span className="inline-flex items-center justify-center p-3 bg-blue-500 rounded-md shadow-xl transition duration-500 ease-in-out group-hover:bg-gray-900">
-                      <h3 className="mb-0 text-lg font-medium text-white tracking-tight group-hover:text-white">
-                        Flexible routes
-                      </h3>
-                    </span>
-                  </div>
-                  <p className="mt-5 text-base text-white group-hover:text-gray-900">
-                    Ac tincidunt sapien vehicula erat auctor pellentesque
-                    rhoncus. Et magna sit morbi lobortis.
-                  </p>
-                </div>
-              </div>
-            </div>
+export const WhyDriveForUs = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      wpPage(title: { eq: "Recruitment Landing Page" }) {
+        recruitment {
+          benefits {
+            to
+            title
+            description
+            benefits {
+              title
+              description
+            }
+          }
+        }
+      }
+    }
+  `);
 
-            <div className="pt-6">
-              <div className="flow-root bg-gray-900 rounded-lg px-6 pb-8">
-                <div className="-mt-6">
-                  <div>
-                    <span className="inline-flex items-center justify-center p-3 bg-blue-500 rounded-md shadow-lg">
-                      <h3 className="mb-0 text-lg font-medium text-white tracking-tight">
-                        Flexible schedule
-                      </h3>
-                    </span>
-                  </div>
-                  <p className="mt-5 text-base text-white">
-                    Ac tincidunt sapien vehicula erat auctor pellentesque
-                    rhoncus. Et magna sit morbi lobortis.
-                  </p>
-                </div>
-              </div>
-            </div>
+  const { to, title, description, benefits } = data.wpPage.recruitment.benefits;
 
-            <div className="pt-6">
-              <div className="flow-root bg-gray-900 rounded-lg px-6 pb-8">
-                <div className="-mt-6">
-                  <div>
-                    <span className="inline-flex items-center justify-center p-3 bg-blue-500 rounded-md shadow-lg">
-                      <h3 className="mb-0 text-lg font-medium text-white tracking-tight">
-                        Inhouse accountant
-                      </h3>
-                    </span>
+  return (
+    <>
+      <section className="relative bg-white px-4 section-spacing" id={to}>
+        <div className="mx-auto max-w-md text-center sm:max-w-3xl lg:max-w-7xl">
+          <p className="mt-2 text-3xl font-bold tracking-tight sm:text-4xl">
+            {title}
+          </p>
+          <p className="mt-5 max-w-prose mx-auto text-xl text-gray-500">
+            {description}
+          </p>
+          <div className="mt-12">
+            <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+              {benefits.map((benefit) => (
+                <div className="pt-6">
+                  <div className="flow-root bg-gray-900 rounded-lg px-6 pb-8">
+                    <div className="-mt-6">
+                      <div>
+                        <span className="inline-flex items-center justify-center p-3 bg-blue-500 rounded-md shadow-lg">
+                          <h3 className="mb-0 text-lg font-medium text-white tracking-tight">
+                            {benefit.title}
+                          </h3>
+                        </span>
+                      </div>
+                      <p className="mt-5 text-base text-white">
+                        {benefit.description}
+                      </p>
+                    </div>
                   </div>
-                  <p className="mt-5 text-base text-white">
-                    Ac tincidunt sapien vehicula erat auctor pellentesque
-                    rhoncus. Et magna sit morbi lobortis.
-                  </p>
                 </div>
-              </div>
-            </div>
-
-            <div className="pt-6">
-              <div className="flow-root bg-gray-900 rounded-lg px-6 pb-8">
-                <div className="-mt-6">
-                  <div>
-                    <span className="inline-flex items-center justify-center p-3 bg-blue-500 rounded-md shadow-lg">
-                      <h3 className="mb-0 text-lg font-medium text-white tracking-tight">
-                        Opportunity for growth
-                      </h3>
-                    </span>
-                  </div>
-                  <p className="mt-5 text-base text-white">
-                    Ac tincidunt sapien vehicula erat auctor pellentesque
-                    rhoncus. Et magna sit morbi lobortis.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="pt-6">
-              <div className="flow-root bg-gray-900 rounded-lg px-6 pb-8">
-                <div className="-mt-6">
-                  <div>
-                    <span className="inline-flex items-center justify-center p-3 bg-blue-500 rounded-md shadow-lg">
-                      <h3 className="mb-0 text-lg font-medium text-white tracking-tight">
-                        Opportunity for growth
-                      </h3>
-                    </span>
-                  </div>
-                  <p className="mt-5 text-base text-white">
-                    Ac tincidunt sapien vehicula erat auctor pellentesque
-                    rhoncus. Et magna sit morbi lobortis.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="pt-6">
-              <div className="flow-root bg-gray-900 rounded-lg px-6 pb-8">
-                <div className="-mt-6">
-                  <div>
-                    <span className="inline-flex items-center justify-center p-3 bg-blue-500 rounded-md shadow-lg">
-                      <h3 className="mb-0 text-lg font-medium text-white tracking-tight">
-                        Opportunity for growth
-                      </h3>
-                    </span>
-                  </div>
-                  <p className="mt-5 text-base text-white">
-                    Ac tincidunt sapien vehicula erat auctor pellentesque
-                    rhoncus. Et magna sit morbi lobortis.
-                  </p>
-                </div>
-              </div>
+              ))}
             </div>
           </div>
         </div>
-      </div>
-    </section>
-  </>
-);
+      </section>
+    </>
+  );
+};
