bbd673d1f4f20c12140d8524c5358ba33a71cba9
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-06-15 18:58:40 -0600

    Pull data from backend

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,45 +1,51 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 
-export const FAQ = () => (
-  <>
-    <section className="bg-gray-900 mx-4 rounded-lg section-spacing" id="FAQ">
-      <div className="px-4 mx-auto">
-        <div className="lg:max-w-2xl lg:mx-auto lg:text-center">
-          <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-            Frequently asked questions
-          </h2>
-          <p className="mt-4 text-gray-400">
-            Ac euismod vel sit maecenas id pellentesque eu sed consectetur.
-            Malesuada adipiscing sagittis vel nulla nec. Urna, sed a lectus
-            elementum blandit et.
-          </p>
-        </div>
-        <div className="mt-20">
-          <dl className="space-y-10 lg:space-y-0 lg:grid lg:grid-cols-2 lg:gap-x-8 lg:gap-y-10">
-            <div>
-              <dt className="font-semibold text-white">
-                What&#039;s the best thing about Switzerland?
-              </dt>
-              <dd className="mt-3 text-gray-400">
-                I don&#039;t know, but the flag is a big plus. Lorem ipsum dolor
-                sit amet consectetur adipisicing elit. Quas cupiditate
-                laboriosam fugiat.
-              </dd>
-            </div>
+export const FAQ = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      wpPage(title: { eq: "Recruitment Landing Page" }) {
+        recruitment {
+          faq {
+            to
+            title
+            description
+            questions {
+              question
+              answer
+            }
+          }
+        }
+      }
+    }
+  `);
 
-            <div>
-              <dt className="font-semibold text-white">
-                What&#039;s the best thing about Switzerland?
-              </dt>
-              <dd className="mt-3 text-gray-400">
-                I don&#039;t know, but the flag is a big plus. Lorem ipsum dolor
-                sit amet consectetur adipisicing elit. Quas cupiditate
-                laboriosam fugiat.
-              </dd>
-            </div>
-          </dl>
+  const { to, title, description, questions } = data.wpPage.recruitment.faq;
+
+  return (
+    <>
+      <section className="bg-gray-900 mx-4 rounded-lg section-spacing" id={to}>
+        <div className="px-4 mx-auto">
+          <div className="lg:max-w-2xl lg:mx-auto lg:text-center">
+            <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+              {title}
+            </h2>
+            <p className="mt-4 text-gray-400">{description}</p>
+          </div>
+          <div className="mt-20">
+            <dl className="space-y-10 lg:space-y-0 lg:grid lg:grid-cols-2 lg:gap-x-8 lg:gap-y-10">
+              {questions.map((question) => (
+                <div>
+                  <dt className="font-semibold text-white">
+                    {question.question}
+                  </dt>
+                  <dd className="mt-3 text-gray-400">{question.answer}</dd>
+                </div>
+              ))}
+            </dl>
+          </div>
         </div>
-      </div>
-    </section>
-  </>
-);
+      </section>
+    </>
+  );
+};
--- src/components/job-openings.js
+++ src/components/job-openings.js
@@ -1,56 +1,105 @@
 import React, { Fragment } from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 
-export const JobOpenings = () => (
-  <>
-    <section
-      className="relative bg-white mx-4 py-16 sm:py-24 lg:py-32"
-      id="Job Openings"
-    >
-      <div className="mx-auto max-w-md text-center sm:max-w-3xl sm:px-0 lg:max-w-7xl">
-        <p className="mt-2 text-3xl font-bold tracking-tight sm:text-4xl">
-          Open roles
-        </p>
-        <p className="mt-5 max-w-prose mx-auto text-xl text-gray-500">
-          Phasellus lorem quam molestie id quisque diam aenean nulla in.
-          Accumsan in quis quis nunc, ullamcorper malesuada. Eleifend
-          condimentum id viverra nulla.
-        </p>
-        <div className="mt-12">
-          <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-2">
-            <div className="pt-6">
-              <div className="flow-root bg-gray-900 rounded-lg shadow-lg px-6 pb-8">
-                <div>
-                  <h2 className="text-3xl text-white font-medium mt-4">
-                    Company Driver
-                  </h2>
-                  <div className="flex flex-col justify-between	h-auto max-h-48">
-                    <p className="mt-5 text-md text-white mb-6">
-                      Click 'Apply Now' to fill out the job application. We have
-                      4 positions left.
-                    </p>
-                    <div className="rounded-md shadow">
-                      <a className="cursor-pointer uppercase w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-blue-700 bg-gray-50 hover:bg-gray-200 md:py-4 lg:text-lg md:px-10">
-                        Apply Now
-                      </a>
+export const JobOpenings = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      wpPage(title: { eq: "Recruitment Landing Page" }) {
+        recruitment {
+          openPositions {
+            to
+            title
+            description
+            companyDriver {
+              title
+              description
+              ctaButtons {
+                text
+                link
+              }
+            }
+            ownerOperator {
+              title
+              description
+              ctaButtons {
+                text
+                link
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const {
+    to,
+    title,
+    description,
+    companyDriver,
+    ownerOperator,
+  } = data.wpPage.recruitment.openPositions;
+
+  return (
+    <>
+      <section
+        className="relative bg-white mx-4 py-16 sm:py-24 lg:py-32"
+        id={to}
+      >
+        <div className="mx-auto max-w-md text-center sm:max-w-3xl sm:px-0 lg:max-w-7xl">
+          <p className="mt-2 text-3xl font-bold tracking-tight sm:text-4xl">
+            {title}
+          </p>
+          <p className="mt-5 max-w-prose mx-auto text-xl text-gray-500">
+            {description}
+          </p>
+          <div className="mt-12">
+            <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-2">
+              <div className="pt-6">
+                <div className="flow-root bg-gray-900 rounded-lg shadow-lg px-6 pb-8">
+                  <div>
+                    <h2 className="text-3xl text-white font-medium mt-4">
+                      {companyDriver.title}
+                    </h2>
+                    <div className="flex flex-col justify-between	h-auto max-h-48">
+                      <p className="mt-5 text-md text-white mb-6">
+                        {companyDriver.description}
+                      </p>
+                      <div className="rounded-md shadow">
+                        <a
+                          href={companyDriver.ctaButtons.link}
+                          target="_blank"
+                          rel="noreferrer"
+                          className="cursor-pointer uppercase w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-blue-700 bg-gray-50 hover:bg-gray-200 md:py-4 lg:text-lg md:px-10"
+                        >
+                          {companyDriver.ctaButtons.text}
+                        </a>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
 
-            <div className="pt-6">
-              <div className="flow-root bg-gray-100 rounded-lg shadow-lg px-6 pb-8">
-                <div>
-                  <h2 className="text-3xl  font-medium mt-4">Owner Operator</h2>
-                  <div className="flex flex-col justify-between	h-auto max-h-48">
-                    <p className="mt-5 text-md text-gray-900 mb-6">
-                      Click 'Apply Now' to fill out the job application. We have
-                      4 positions left.
-                    </p>
-                    <div className="rounded-md shadow">
-                      <a className="cursor-pointer uppercase w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-600 md:py-4 lg:text-lg md:px-10">
-                        Talk to sales
-                      </a>
+              <div className="pt-6">
+                <div className="flow-root bg-gray-100 rounded-lg shadow-lg px-6 pb-8">
+                  <div>
+                    <h2 className="text-3xl  font-medium mt-4">
+                      {ownerOperator.title}
+                    </h2>
+                    <div className="flex flex-col justify-between	h-auto max-h-48">
+                      <p className="mt-5 text-md text-gray-900 mb-6">
+                        {ownerOperator.description}
+                      </p>
+                      <div className="rounded-md shadow">
+                        <a
+                          href={ownerOperator.ctaButtons.link}
+                          target="_blank"
+                          rel="noreferrer"
+                          className="cursor-pointer uppercase w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-600 md:py-4 lg:text-lg md:px-10"
+                        >
+                          {ownerOperator.ctaButtons.text}
+                        </a>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -58,7 +107,7 @@ export const JobOpenings = () => (
             </div>
           </div>
         </div>
-      </div>
-    </section>
-  </>
-);
+      </section>
+    </>
+  );
+};
