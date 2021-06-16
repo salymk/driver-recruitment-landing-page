2c6be46d20de8b46a2a9d5118cb5549ded718755
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-06-15 19:02:02 -0600

    use seocontext for seo optimization

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,17 +7,92 @@
 
 import * as React from 'react';
 import PropTypes from 'prop-types';
+import { useStaticQuery, graphql } from 'gatsby';
+import { SEOContext } from 'gatsby-plugin-wpgraphql-seo';
 
 import { Header } from './header';
 import { Footer } from './footer';
 
-const Layout = ({ children }) => (
-  <>
-    <Header />
-    <main className="max-w-6xl m-auto">{children}</main>
-    <Footer />
-  </>
-);
+const Layout = ({ children }) => {
+  const {
+    wp: { seo },
+  } = useStaticQuery(graphql`
+    query SiteInfoQuery {
+      wp {
+        seo {
+          contentTypes {
+            page {
+              metaDesc
+              metaRobotsNoindex
+              schemaType
+              title
+            }
+          }
+          webmaster {
+            googleVerify
+            yandexVerify
+            msVerify
+            baiduVerify
+          }
+          schema {
+            companyName
+            personName
+            companyOrPerson
+            wordpressSiteName
+            siteUrl
+            siteName
+            inLanguage
+            logo {
+              sourceUrl
+              mediaItemUrl
+              altText
+            }
+          }
+          social {
+            facebook {
+              url
+              defaultImage {
+                sourceUrl
+                mediaItemUrl
+              }
+            }
+            instagram {
+              url
+            }
+            linkedIn {
+              url
+            }
+            mySpace {
+              url
+            }
+            pinterest {
+              url
+              metaTag
+            }
+            twitter {
+              username
+            }
+            wikipedia {
+              url
+            }
+            youTube {
+              url
+            }
+          }
+        }
+      }
+    }
+  `);
+  return (
+    <>
+      <SEOContext.Provider value={{ global: seo }}>
+        <Header />
+        <main className="max-w-6xl m-auto">{children}</main>
+        <Footer />
+      </SEOContext.Provider>
+    </>
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
