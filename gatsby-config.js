f38719fcf5444e4ae3aeffcfede535c5e32a965e
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-11-07 13:07:57 -0700

    Comment out gatsby-plugin-google-analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,20 +9,20 @@ module.exports = {
     author: `Umar Mitchell`,
   },
   plugins: [
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: 'G-NP37G0LW75',
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ['/preview/**', '/do-not-track/me/too/'],
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     // The property ID; the tracking code won't be generated without it
+    //     trackingId: 'G-NP37G0LW75',
+    //     // Defines where to place the tracking script - `true` in the head and `false` in the body
+    //     head: true,
+    //     // Avoids sending pageview hits from custom paths
+    //     exclude: ['/preview/**', '/do-not-track/me/too/'],
 
-        // defaults to false
-        enableWebVitalsTracking: true,
-      },
-    },
+    //     // defaults to false
+    //     enableWebVitalsTracking: true,
+    //   },
+    // },
     {
       resolve: `gatsby-source-wordpress`,
       options: {
